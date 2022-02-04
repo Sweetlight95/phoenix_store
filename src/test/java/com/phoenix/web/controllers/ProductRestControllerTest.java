b2067361e38c23568e42411a0e39d61ec96c9f27
Sweetlight95 <princessadesuwa93@gmail.com>
2022-02-04 17:17:51 +0100

    updated

--- src/test/java/com/phoenix/web/controllers/ProductRestControllerTest.java
+++ src/test/java/com/phoenix/web/controllers/ProductRestControllerTest.java
@@ -1,5 +1,6 @@
 package com.phoenix.web.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.phoenix.data.models.Product;
 import com.phoenix.data.repository.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +11,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -26,23 +32,53 @@ class ProductRestControllerTest {
     @Autowired
     ProductRepository productRepository;
 
+    ObjectMapper objectMapper;
+
     @BeforeEach
     void setUp() {
+        objectMapper = new ObjectMapper();
     }
 
     @Test
     @DisplayName("Get product api test")
     void getProductsTest() throws Exception {
+        mockMvc.perform(get("/api/product")
+                        .contentType("application/json"))
+                        .andExpect(status().is(200))
+                        .andDo(print());
+    }
+
+
+    @Test
+    @DisplayName("Create a product api test")
+    void createProductsTest() throws Exception {
         Product product = new Product();
         product.setName("Bamboo Chair");
         product.setDescription("World class bamboo");
         product.setPrice(5540);
         product.setQuantity(9);
 
-        mockMvc.perform(get("/api/product")
-                .contentType("application/json"))
+        String requestBody = objectMapper.writeValueAsString(product);
+
+        mockMvc.perform(post("/api/product")
+                .contentType("application/json").content(requestBody))
                 .andExpect(status().is(200))
                 .andDo(print());
+    }
+
+    @Test
+    void updateProductsTest() throws Exception{
+        Product product = productRepository.findById(14L).orElse(null);
+        assertThat(product).isNotNull();
+
+        mockMvc.perform(patch("/api/product/14")
+                        .contentType("application/json-patch+json").content(Files.readAllBytes(Path.of("PayLoan.json"))))
+                .andExpect(status().is(200))
+                .andDo(print());
+
+        product = productRepository.findById(14L).orElse(null);
+        assertThat(product).isNotNull();
+        assertThat(product.getDescription()).isEqualTo("This is a bamboo");
     }
 
 }
